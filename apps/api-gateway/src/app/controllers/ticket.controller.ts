4f3c86741ab5e41b044cf3853301843082b31122
Rohit Karthik <rohitismaster@gmail.com>
2025-01-03 16:20:11 -0500

    changes

--- apps/api-gateway/src/app/controllers/ticket.controller.ts
+++ apps/api-gateway/src/app/controllers/ticket.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Headers, HttpStatus, Logger, Post, Res } from "@nestjs/common";
+import { Body, Controller, Get, Headers, HttpStatus, Logger, Post, Res } from "@nestjs/common";
 import { Inject } from "@nestjs/common";
 import { ClientProxy } from "@nestjs/microservices";
 import { Response } from "express";
@@ -8,119 +8,148 @@ import { AuthService } from "@backend/libs";
 
 @Controller("ticket")
 export class TicketController {
-    constructor(
+  constructor(
     @Inject("CLIENT_PROXY") private readonly clientProxy: ClientProxy,
     @Inject(TicketService) private readonly ticketService: TicketService,
     @Inject(AuthService) private readonly authService: AuthService
-    ) {}
+  ) { }
 
-    @Post("get")
-    async getTicketInfo(
-        @Body() body: {
-            ticketId: string
-        },
-        @Headers() headers,
-        @Res() response: Response
-    ) {
-        try {
-            const token = headers["authorization"];
+  @Get("getMany")
+  async getManyTickets(
+    @Headers() headers,
+    @Res() response: Response
+  ) {
+    try {
+      const token = headers["authorization"];
 
-            if (!(body) || !(body.ticketId)) {
-                return response.status(HttpStatus.BAD_REQUEST).json({data:'error',error:'malformed body'});
-            }
+      const result = await this.ticketService.getManyTickets({
+        token: token,
+      }) as any;
 
-            const result = await this.ticketService.getTicket({
-                token: token,
-                ...body
-            }) as any;
+      if ((result) && (result.error)) {
+        return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ data: 'error', error: result.error });
+      }
 
-            if ((result) && (result.error)) {
-                return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({data:'error',error:result.error});
-            }
+      if (result.value) {
+        return response.json(result.value);
+      }
 
-            if (result.value) {
-                return response.json(result.value);
-            }
+      return response.json(result);
 
-            return response.json(result);
-        } catch(e) {
-            Logger.error(e);
-            return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({data:'error',error:e});
-        }
+
+    } catch (e) {
+      Logger.error(e);
+      return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ data: 'error', error: e });
+    }
+  }
+
+  @Post("get")
+  async getTicketInfo(
+    @Body() body: {
+      ticketId: string
+    },
+    @Headers() headers,
+    @Res() response: Response
+  ) {
+    try {
+      const token = headers["authorization"];
+
+      if (!(body) || !(body.ticketId)) {
+        return response.status(HttpStatus.BAD_REQUEST).json({ data: 'error', error: 'malformed body' });
+      }
+
+      const result = await this.ticketService.getTicket({
+        token: token,
+        ...body
+      }) as any;
+
+      if ((result) && (result.error)) {
+        return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ data: 'error', error: result.error });
+      }
+
+      if (result.value) {
+        return response.json(result.value);
+      }
+
+      return response.json(result);
+    } catch (e) {
+      Logger.error(e);
+      return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ data: 'error', error: e });
+    }
+  }
+
+  @Post("scan")
+  async scanTicket(
+    @Body() body: {
+      ticketId: string;
+      concertId: string;
+    },
+    @Headers() headers,
+    @Res() response: Response
+  ) {
+    try {
+      const token = headers["authorization"];
+
+      const result = await this.ticketService.scanTicket({
+        token: token,
+        ...body
+      }) as any;
+
+      if ((result) && (result.error)) {
+        return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ data: 'error', error: result.error });
+      }
+
+      if (result.value) {
+        return response.json(result.value);
+      }
+
+      return response.json(result);
+
+    } catch (e) {
+      Logger.error(e);
+      return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ data: 'error', error: e });
+    }
+  }
+
+  @Post("purchase")
+  async purchaseTickets(
+    @Body() body: {
+      // userId: string,
+      concertId: string,
+      quantity: number
+    },
+    @Headers() headers,
+    @Res() response: Response
+  ) {
+    const token = headers["authorization"];
+    Logger.log(token)
+    if (!(token)) {
+      return response.status(HttpStatus.FORBIDDEN).json({ data: "forbidden" })
     }
 
-    @Post("scan")
-    async scanTicket(
-        @Body() body: {
-            ticketId: string;
-            concertId: string;
-        },
-        @Headers() headers,
-        @Res() response: Response
-    ) {
-        try {
-            const token = headers["authorization"];
-
-            const result = await this.ticketService.scanTicket({
-                token: token,
-                ...body
-            }) as any;
-
-            if ((result) && (result.error)) {
-                return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({data:'error',error:result.error});
-            }
-
-            if (result.value) {
-                return response.json(result.value);
-            }
-
-            return response.json(result);
-
-        } catch(e) {
-            Logger.error(e);
-            return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({data:'error',error:e});
-        }
+    if (!(body) || !(body.concertId) || !(body.quantity)) {
+      return response.status(HttpStatus.BAD_REQUEST).json({ data: "Bad Request" });
     }
 
-    @Post("purchase")
-    async purchaseTickets(
-        @Body() body: {
-        // userId: string,
-        concertId: string,
-        quantity: number
-        },
-        @Headers() headers,
-        @Res() response: Response
-    ) {
-        const token = headers["authorization"];
-        Logger.log(token)
-        if (!(token)) {
-            return response.status(HttpStatus.FORBIDDEN).json({data:"forbidden"})
-        }
+    const verify = this.authService.verifyJwt(token);
 
-        if (!(body) || !(body.concertId) || !(body.quantity)) {
-            return response.status(HttpStatus.BAD_REQUEST).json({data:"Bad Request"});
-        }
-
-        const verify = this.authService.verifyJwt(token);
-
-        if (verify.statusCode == 1) {
-            return response.status(HttpStatus.FORBIDDEN).json({data:"Forbidden"});
-        }
-
-        const userId = verify.payload.userId;
-
-        if (!(userId)) {
-            return response.status(HttpStatus.BAD_REQUEST).json({data:"Bad Request"});
-        }
-
-        const TicketPurchasePayload = new TicketPurchaseDto({
-            userId: userId,
-            concertId: body.concertId,
-            quantity: body.quantity
-        });
-        const result = await this.ticketService.purchaseTickets(token, TicketPurchasePayload);
-
-        return response.json(result.unwrapOr({data:'error',error:'default error'}));
+    if (verify.statusCode == 1) {
+      return response.status(HttpStatus.FORBIDDEN).json({ data: "Forbidden" });
     }
+
+    const userId = verify.payload.userId;
+
+    if (!(userId)) {
+      return response.status(HttpStatus.BAD_REQUEST).json({ data: "Bad Request" });
+    }
+
+    const TicketPurchasePayload = new TicketPurchaseDto({
+      userId: userId,
+      concertId: body.concertId,
+      quantity: body.quantity
+    });
+    const result = await this.ticketService.purchaseTickets(token, TicketPurchasePayload);
+
+    return response.json(result.unwrapOr({ data: 'error', error: 'default error' }));
+  }
 }
--- apps/api-gateway/src/app/services/ticket/ticket.service.ts
+++ apps/api-gateway/src/app/services/ticket/ticket.service.ts
@@ -7,189 +7,233 @@ import { Concert, ConcertAttendence, Ticket } from "@prisma/client";
 
 @Injectable()
 export class TicketService {
-    constructor(
-        @Inject("CLIENT_PROXY") private readonly clientProxy: ClientProxy,
-        @Inject(AuthService) private readonly authService: AuthService,
-        @Inject(PrismaService) private readonly prisma: PrismaService
-    ) {
-        //
-    }
+  constructor(
+    @Inject("CLIENT_PROXY") private readonly clientProxy: ClientProxy,
+    @Inject(AuthService) private readonly authService: AuthService,
+    @Inject(PrismaService) private readonly prisma: PrismaService
+  ) {
+    //
+  }
 
-    public async getTicket(payload: { token: string, ticketId: string }): Promise<Result<{
-        user?: {
-            email: string;
-            id: string;
-            name: string;
+  public async getManyTickets(payload: {
+    token: string
+  }): Promise<Result<{
+    tickets: Ticket[]
+  } & ResultInterface<string>, ResultInterface<string>>> {
+    try {
+      const { token } = payload;
+
+      const verify = this.authService.verifyJwt(token);
+
+      if (verify.statusCode == 1) {
+        return new Err({ data: 'error', error: 'JWT verification error' });
+      }
+
+      if (verify.isValid == false) {
+        return new Err({ data: 'error', error: 'Invalid JWT' });
+      }
+
+      if (!(verify.payload)) {
+        return new Err({ data: 'error', error: "Unreadable JWT payload" });
+      }
+
+      if (!(verify.payload.userId)) {
+        return new Err({ data: 'error', error: "User ID not in JWT payload" });
+      }
+
+      const tickets = await this.prisma.ticket.findMany({
+        where: {
+          userId: verify.payload.userId
+        }
+      });
+
+      return new Ok({
+        data: "success",
+        tickets: tickets,
+      })
+
+
+    } catch (e) {
+      Logger.error(e);
+      return new Err({ data: 'error', error: e });
+    }
+  }
+
+  public async getTicket(payload: { token: string, ticketId: string }): Promise<Result<{
+    user?: {
+      email: string;
+      id: string;
+      name: string;
+    },
+    concert?: Concert,
+    ticket?: Ticket
+  } & ResultInterface<string>, ResultInterface<string>>> {
+    try {
+      const { token, ticketId } = payload;
+
+      const verify = this.authService.verifyJwt(token);
+
+      if (verify.statusCode == 1) {
+        return new Err({ data: 'error', error: 'JWT verification error' });
+      }
+
+      if (verify.isValid == false) {
+        return new Err({ data: 'error', error: 'Invalid JWT' });
+      }
+
+      if (!(verify.payload)) {
+        return new Err({ data: 'error', error: "Unreadable JWT payload" });
+      }
+
+      if (!(verify.payload.userId)) {
+        return new Err({ data: 'error', error: "User ID not in JWT payload" });
+      }
+
+      const ticketExists = await this.prisma.ticket.findFirst({
+        where: {
+          id: ticketId
+        }
+      });
+
+      if (!(ticketExists)) {
+        return new Err({ data: 'error', error: "Ticket not found" });
+      }
+
+      if (!(ticketExists.userId == verify.payload.userId)) {
+        return new Err({ data: 'error', error: "Unauthorized" });
+      }
+
+      const concertDetails = await this.prisma.concert.findFirst({
+        where: {
+          id: ticketExists.concertId
+        }
+      });
+
+      if (!(concertDetails)) {
+        return new Err({ data: 'error', error: "Concert not found" });
+      }
+
+      const userDetails = await this.prisma.user.findFirst({
+        where: {
+          id: verify.payload.userId
         },
-        concert?: Concert,
-        ticket?: Ticket
-    }&ResultInterface<string>,ResultInterface<string>>> {
-        try {
-            const { token, ticketId } = payload;
-
-            const verify = this.authService.verifyJwt(token);
-
-            if (verify.statusCode == 1) {
-                return new Err({data:'error',error:'JWT verification error'});
-            }
-
-            if (verify.isValid == false) {
-                return new Err({data:'error',error:'Invalid JWT'});
-            }
-
-            if (!(verify.payload)) {
-                return new Err({data:'error',error:"Unreadable JWT payload"});
-            }
-
-            if (!(verify.payload.userId)) {
-                return new Err({data:'error',error:"User ID not in JWT payload"});
-            }
-
-            const ticketExists = await this.prisma.ticket.findFirst({
-                where: {
-                    id: ticketId
-                }
-            });
-
-            if (!(ticketExists)) {
-                return new Err({data:'error',error:"Ticket not found"});
-            }
-
-            if (!(ticketExists.userId == verify.payload.userId)) {
-                return new Err({data:'error',error:"Unauthorized"});
-            }
-
-            const concertDetails = await this.prisma.concert.findFirst({
-                where: {
-                    id: ticketExists.concertId 
-                }
-            });
-
-            if (!(concertDetails)) {
-                return new Err({data:'error',error:"Concert not found"});
-            }
-
-            const userDetails = await this.prisma.user.findFirst({
-                where: {
-                    id: verify.payload.userId
-                },
-                select: {
-                    id: true,
-                    name: true,
-                    email: true
-                }
-            });
-
-
-            if (!(userDetails)) {
-                return new Err({data:'error',error:"User not found"});
-            }
-
-            return new Ok({
-                data: "success",
-                ticket: ticketExists,
-                concert: concertDetails,
-                user: userDetails
-            })
-        } catch(e) {
-            Logger.error(e);
-            return new Err({data:'error',error:e});
+        select: {
+          id: true,
+          name: true,
+          email: true
         }
+      });
+
+
+      if (!(userDetails)) {
+        return new Err({ data: 'error', error: "User not found" });
+      }
+
+      return new Ok({
+        data: "success",
+        ticket: ticketExists,
+        concert: concertDetails,
+        user: userDetails
+      })
+    } catch (e) {
+      Logger.error(e);
+      return new Err({ data: 'error', error: e });
     }
+  }
 
-    public async scanTicket(payload: {token: string, ticketId: string, concertId: string}): Promise<Result<{concertAttendence?:ConcertAttendence}&ResultInterface<string>,ResultInterface<string>>> {
-        try {
-            const { token, ticketId, concertId } = payload;
+  public async scanTicket(payload: { token: string, ticketId: string, concertId: string }): Promise<Result<{ concertAttendence?: ConcertAttendence } & ResultInterface<string>, ResultInterface<string>>> {
+    try {
+      const { token, ticketId, concertId } = payload;
 
-            const verify = this.authService.verifyJwt(token);
+      const verify = this.authService.verifyJwt(token);
 
-            if (verify.statusCode == 1) {
-                return new Err({data:'error',error:'JWT verification error'});
-            }
+      if (verify.statusCode == 1) {
+        return new Err({ data: 'error', error: 'JWT verification error' });
+      }
 
-            if (verify.isValid == false) {
-                return new Err({data:'error',error:'Invalid JWT'});
-            }
+      if (verify.isValid == false) {
+        return new Err({ data: 'error', error: 'Invalid JWT' });
+      }
 
-            if (!(verify.payload)) {
-                return new Err({data:'error',error:"Unreadable JWT payload"});
-            }
+      if (!(verify.payload)) {
+        return new Err({ data: 'error', error: "Unreadable JWT payload" });
+      }
 
-            if (!(verify.payload.userId)) {
-                return new Err({data:'error',error:"User ID not in JWT payload"});
-            }
+      if (!(verify.payload.userId)) {
+        return new Err({ data: 'error', error: "User ID not in JWT payload" });
+      }
 
-            if (!(verify.payload.isAdmin) || (verify.payload.isAdmin == false)) {
-                return new Err({data:'error',error:"Unauthorized"});
-            }
+      if (!(verify.payload.isAdmin) || (verify.payload.isAdmin == false)) {
+        return new Err({ data: 'error', error: "Unauthorized" });
+      }
 
-            const ticketExists = await this.prisma.ticket.findFirst({
-                where: {
-                    id: ticketId
-                }
-            });
-
-            if (!(ticketExists)) {
-                return new Err({data:'error',error:"Ticket not found"});
-            }
-
-            const concertExists = await this.prisma.concert.findFirst({
-                where: {
-                    id: concertId
-                }
-            });
-
-            if (!(concertExists)) {
-                return new Err({data:'error',error:"Concert not found"});
-            }
-
-            const alreadyInAttendence = await this.prisma.concertAttendence.findFirst({
-                where: {
-                    ticketId: ticketId,
-                    concertId: concertId
-                }
-            });
-
-            if (alreadyInAttendence) {
-                return new Err({data:"error",error:"Ticket already scanned"});
-            }
-
-            const newAttendence = await this.prisma.concertAttendence.create({
-                data: {
-                    ticketId: ticketId,
-                    concertId: concertId
-                }
-            });
-
-            return new Ok({data:"success",concertAttendence:newAttendence});
-
-        } catch(e) {
-            Logger.error(e);
-            return new Err({data:'error',error:e});
+      const ticketExists = await this.prisma.ticket.findFirst({
+        where: {
+          id: ticketId
         }
-    }
+      });
 
-    public async purchaseTickets(token: string, payload: TicketPurchaseDto): Promise<Result<ResultInterface<string>, ResultInterface<string>>> {
-        try {
-            const unpacked = payload.unpack();
-            await this.clientProxy.connect();
+      if (!(ticketExists)) {
+        return new Err({ data: 'error', error: "Ticket not found" });
+      }
 
-            const res = await lastValueFrom(this.clientProxy.send<ResultInterface<string>>("PROCESS_TICKETS_PURCHASE", {
-                token: token,
-                payload: unpacked
-            }));
-
-            await this.clientProxy.close();
-
-            if (res.data=='error') {
-                return new Err({data:'error',error:res.error});
-            }
-
-            return new Ok({data:'success'});
-
-        } catch(e) {
-            Logger.error(e);
-            return new Err({data:'error',error:e});
+      const concertExists = await this.prisma.concert.findFirst({
+        where: {
+          id: concertId
         }
+      });
+
+      if (!(concertExists)) {
+        return new Err({ data: 'error', error: "Concert not found" });
+      }
+
+      const alreadyInAttendence = await this.prisma.concertAttendence.findFirst({
+        where: {
+          ticketId: ticketId,
+          concertId: concertId
+        }
+      });
+
+      if (alreadyInAttendence) {
+        return new Err({ data: "error", error: "Ticket already scanned" });
+      }
+
+      const newAttendence = await this.prisma.concertAttendence.create({
+        data: {
+          ticketId: ticketId,
+          concertId: concertId
+        }
+      });
+
+      return new Ok({ data: "success", concertAttendence: newAttendence });
+
+    } catch (e) {
+      Logger.error(e);
+      return new Err({ data: 'error', error: e });
     }
+  }
+
+  public async purchaseTickets(token: string, payload: TicketPurchaseDto): Promise<Result<ResultInterface<string>, ResultInterface<string>>> {
+    try {
+      const unpacked = payload.unpack();
+      await this.clientProxy.connect();
+
+      const res = await lastValueFrom(this.clientProxy.send<ResultInterface<string>>("PROCESS_TICKETS_PURCHASE", {
+        token: token,
+        payload: unpacked
+      }));
+
+      await this.clientProxy.close();
+
+      if (res.data == 'error') {
+        return new Err({ data: 'error', error: res.error });
+      }
+
+      return new Ok({ data: 'success' });
+
+    } catch (e) {
+      Logger.error(e);
+      return new Err({ data: 'error', error: e });
+    }
+  }
 }
